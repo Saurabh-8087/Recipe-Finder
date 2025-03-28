5c12a5d14716cd023335187df7a949ff3b2cf0fa
Saurabh Rathod <saurabh.r@prometteur.in>
2025-03-28 10:54:35 +0530

    showing the all recipe in html page with help of recipe.js file

--- script/recipe-data.js
+++ script/recipe-data.js
@@ -1,10 +1,10 @@
- async function loadRecipe() {
-
+export async function loadrecipe(){
+ 
   const response = await fetch('https://dummyjson.com/recipes');
-
   const data = await response.json();
-  
+
   return data.recipes;
+
 }
 
-console.log(loadRecipe());
+console.log(loadrecipe());
---
+++ script/recipe.js
@@ -0,0 +1,33 @@
+import { loadrecipe } from "./recipe-data.js";
+
+let recipes = [];
+
+function displayRecipe(recipeArray){
+  const recipeList = document.getElementById('recipeList');
+  recipeList.innerHTML='';
+
+  recipeArray.forEach((recipe)=>{
+
+    const recipeCard = document.createElement('div');
+    recipeCard.classList.add('recipe-card');
+
+    recipeCard.innerHTML=`
+    <img src="${recipe.image}" alt="${recipe.name}">
+      <h3>${recipe.name}</h3>
+      <p>Ingredients: ${recipe.ingredients.join(', ')}</p>
+      <button class="js-details-button  detalis-button">Details</button>
+    `;
+
+    
+    recipeList.appendChild(recipeCard);
+
+  });
+}
+
+async function  initialize(){
+  recipes = await loadrecipe();
+  displayRecipe(recipes);
+
+}
+
+initialize();
